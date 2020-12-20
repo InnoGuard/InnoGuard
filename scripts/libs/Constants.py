f0d6fbc1326a35b4ae1f157322bafa5ecc23df3b
irontarkus95 <michael.ali1995@gmail.com>
2020-12-20 15:53:32 -0400

    Updated gitignore, removed constants.py

--- scripts/libs/Constants.py
+++
@@ -1,9 +0,0 @@
-GC_API_KEY = 'AIzaSyCzVbfttzitLK3-lJlJjueOuGUKsJVYHaw'
-
-
-likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
-                   'LIKELY', 'VERY_LIKELY')
-
-
-TISANE_API_KEY_1 = 'c92ec2bd4f78488abb6b94e3c49792c8'
-TISANE_API_KEY_2 = 'cc82d0a8fd5b4f9f9418ddd8598b1208'
