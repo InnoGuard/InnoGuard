be8bddf2508c0fa02d5c808f24044ee162711be4
irontarkus95 <michael.ali1995@gmail.com>
2020-12-20 15:48:23 -0400

    Added user collection in mongodb

--- scripts/libs/mongodb.py
+++ scripts/libs/mongodb.py
@@ -3,7 +3,7 @@ import pymongo
 Client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = Client["Database"]
 
+users = db["users"]
 browser_responses = db["browser_responses"]
 text_responses = db["text_responses"]
 racy_image_responses = db["racy_image_responses"]
-
